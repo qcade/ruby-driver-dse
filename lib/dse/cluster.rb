0fe0636873122ff46f11af3c705a66c4399539eb
Sandeep Tamhankar <sandeep.tamhankar@datastax.com>
2016-04-08 14:26:05 -0700

    RUBY-192: Wrap Cassandra::Cluster and Cassandra::Session instead of sub-classing them for DSE::*.

--- lib/dse/cluster.rb
+++ lib/dse/cluster.rb
@@ -1,40 +1,38 @@
-require 'forwardable'
-
 module Dse
   # Cluster represents a DSE cluster. It serves as a
   # {Dse::Session session factory} and a collection of metadata. It wraps
   # a {http://datastax.github.io/ruby-driver/api/cluster Cassandra::Cluster} and exposes all of its functionality.
   class Cluster
-    extend Forwardable
-
-    def_delegators :@delegate_cluster, :register, :unregister, :each_host, :hosts, :each_keyspace, :keyspaces,
-                   :refresh_schema_async, :refresh_schema,
-                   :name, :find_replicas,
-                   :SEVERAL_MORE_SYMBOLS_SOME_OF_WHICH_ARE_DELEGATES_THEMSELVES_IN_CLUSTER_LIKE_NAME_AND_FIND_REPLICAS
-
+    # @private
     def initialize(*args)
       @delegate_cluster = Cassandra::Cluster.new(*args)
     end
 
-    def connect_async(*args)
-      # TODO
-    end
-
-    def connect(*args)
-      # TODO
-    end
-
-    # Many cluster methods return "self". So all of those methods need to be implemented here
-    # to delegate to the embedded cluster and then return *our* self.
-    # With some dynamic method creation logic, we can probably do that efficiently, but
-    # it feels like we're going to a lot of trouble to make Dse::Cluster act like a
-    # Cassandra::Cluster without actually being a Cassandra::Cluster.
-    # Several of the entries in def_delegators above are in this category, and don't belong in def_delegators.
-
-
     # @private
-    def new_session(client)
-      Dse::Session.new(client, @execution_options, @futures)
+    def method_missing(method_name, *args, &block)
+      # If we get here, we don't have a method of our own. Forward the request to the delegate_cluster.
+      # If it returns itself, we will coerce the result to return our *self* instead.
+
+      result = @delegate_cluster.send(method_name, *args, &block)
+      (result == @delegate_cluster) ? self : result
+    end
+
+    # Delegates to {http://datastax.github.io/ruby-driver/api/cluster/#connect_async-instance_method Cassandra::Cluster#connect_async}
+    # to connect asynchronously to a cluster, but returns a future that will resolve to a DSE session rather than
+    # Cassandra session.
+    # @return [Cassandra::Future<Dse::Session>]
+    def connect_async(*args)
+      future = @delegate_cluster.connect_async(*args)
+      # We want to actually return a DSE session upon successful connection.
+      future.then do |cassandra_session|
+        Dse::Session.new(cassandra_session)
+      end
+    end
+
+    # Synchronous variant of {#connect_async}.
+    # @return [Dse::Session]
+    def connect(*args)
+      connect_async(*args).get
     end
   end
 end
