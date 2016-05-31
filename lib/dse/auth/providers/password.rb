abcd0c7035c8a42c56cd2682f623ea4e9777a0b0
Sandeep Tamhankar <sandeep.tamhankar@datastax.com>
2016-05-31 16:11:26 -0700

    RUBY-216 - LDAP and Internal auth support for DSE Authenticator

---
+++ lib/dse/auth/providers/password.rb
@@ -0,0 +1,53 @@
+# encoding: utf-8
+
+#--
+# Copyright 2013-2016 DataStax, Inc.
+#++
+
+module Dse
+  module Auth
+    module Providers
+      # Auth provider to authenticate with username/password for DSE's built-in authentication as well as LDAP.
+      #
+      # @note No need to instantiate this class manually, use `:username` and
+      #   `:password` options when calling {Dse.cluster} and one will be
+      #   created automatically for you.
+
+      class Password < Cassandra::Auth::Provider
+        # @private
+        class Authenticator
+          def initialize(authentication_class, username, password)
+            @authentication_class = authentication_class
+            @username = username
+            @password = password
+          end
+
+          def initial_response
+            @authentication_class == 'com.datastax.bdp.cassandra.auth.DseAuthenticator' ?
+                'PLAIN' :
+                challenge_response('PLAIN-START')
+          end
+
+          def challenge_response(token)
+            "\x00#{@username}\x00#{@password}"
+          end
+
+          def authentication_successful(token)
+          end
+        end
+
+        # @param username [String] username to use for authentication to Cassandra
+        # @param password [String] password to use for authentication to Cassandra
+        def initialize(username, password)
+          @username = username
+          @password = password
+        end
+
+        # @private
+        def create_authenticator(authentication_class, host)
+          Authenticator.new(authentication_class, @username, @password)
+        end
+      end
+    end
+  end
+end
