f881b928fb0538147657ee07223edf3bc4a74b85
Sandeep Tamhankar <sandeep.tamhankar@datastax.com>
2016-06-14 16:44:53 -0700

    RUBY-229 - Enable JRuby CI build of DSE driver. * Fixed rake tasks for jruby builds * FIxed broken gss-api spec's; those should be skipped until we add Kerberos support in JRuby.

--- spec/dse/auth/providers/gss_api_spec.rb
+++ spec/dse/auth/providers/gss_api_spec.rb
@@ -19,6 +19,8 @@ module Dse
             x
           end
 
+          return if RUBY_ENGINE == 'jruby'
+
           it 'should default to a NameInfoResolver' do
             provider = GssApi.new('foo')
             expect(provider.instance_variable_get(:@host_resolver)).to be_instance_of(GssApi::NameInfoResolver)
